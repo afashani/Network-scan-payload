8d176bb0b68a153a123d4d4d807f5828ee664d5c
afashani <afashani@gmail.com>
2023-04-30 15:12:56 +0530

    network scanning for open ports

---
+++ testscan.py
@@ -0,0 +1,36 @@
+from scapy.all import *
+
+target_ip = "192.168.8.153"
+
+port_range = (1, 1024)
+
+
+
+
+
+open_ports = []
+
+for port in range(port_range[0], port_range[1] + 1):
+
+    syn_packet = IP(dst=target_ip)/TCP(dport=port, flags="S")
+
+    response = sr1(syn_packet, timeout=1, verbose=0)
+
+    if response and TCP in response and response[TCP].flags == "SA":
+
+        open_ports.append(port)
+
+
+
+#Print the results
+
+
+
+if len(open_ports) == 0:
+
+    print("No open ports found.")
+
+else:
+
+    print(f"Open ports: {', '.join(str(port) for port in open_ports)}")
+
